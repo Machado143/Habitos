2d4fa5c60ab8b1f7394190429c9aea860fb5be36
machado143 <machadogm14@gmail.com>
2026-01-13 14:33:30 -0300

    inicialização do projeto e criação das primeiras classes no models

---
+++ habit_tracker/habits/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Habit(models.Model)
+    FREQUENCY_CHOICES = [
+        ('D', 'Diário'),
+        ('W', 'Semanal'),
+]
+
+user = models.ForeignKey(User, on_delete=models.CASCADE)
+name = models.CharField(max_length=100)
+frequency = models.CharField(max_length=1, choices=FREQUENCY_CHOICES)
+created_at = models.DateTimeField(auto_now_add=True)
+
+def __str__(self):
+    return self.name
+
+class HabitLog(models.Model):
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)    
